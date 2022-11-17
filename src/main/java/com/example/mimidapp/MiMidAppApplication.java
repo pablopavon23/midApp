c2419fc67495b27a7196dee16167f44fbaf1f3f3
pablopavon23 <p.pavon23695@gmail.com>
2022-11-17 11:55:48 +0100

    Initial commit

---
+++ src/main/java/com/example/mimidapp/MiMidAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.mimidapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiMidAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiMidAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mimidapp/MiMidAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mimidapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiMidAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
